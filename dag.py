3c4c474088c284aa5a65f1b530a2b7c8e6f1f4df
Asem Alaa <asem_alla@msn.com>
2021-11-12 20:31:32 +0000

    update

--- dag.py
+++ dag.py
@@ -1,216 +1,259 @@
-import pandas as pd
+from collections import defaultdict
 import json
-import networkx as nx
 import re
 
+import pandas as pd
 
-class SNOMEDDAG:
+import networkx as nx
+
+
+class CCSDAG:
     """
-    A class that wraps SNOMED dictionaries and enable functionalities
-    related to the hierarchical structure of SNOMED CT scheme,
+    A class that wraps CCS dictionaries and enable functionalities
+    related to the hierarchical structure of CCS,
     like retrieving ancestry and descedants.
     """
-    def __init__(self, snomed_cdb_active, snomed_cdb_inactive, ch2pt_json,
-                 pt2ch_json):
-        self.snomed_cdb_df = pd.read_csv(snomed_cdb_active, index_col=0)
-        self.active_terms = set(self.snomed_cdb_df.cui.unique())
-        _df1 = self.snomed_cdb_df[self.snomed_cdb_df.tty == 1].reset_index(
-            drop=True)
+    def __init__(self, diag_single_ccs_file: str, diag_multi_ccs_file: str,
+                 proc_single_ccs_file: str, proc_multi_ccs_file: str):
 
-        _df1 = (_df1.groupby('cui',
-                             as_index=False).agg(name=('str',
-                                                       lambda x: x.values[0])))
+        self.diag_single_ccs_df = pd.read_csv(diag_single_ccs_file, skiprows=1)
+        self.diag_multi_ccs_df = pd.read_csv(diag_multi_ccs_file, skiprows=1)
+        self.proc_single_ccs_df = pd.read_csv(proc_single_ccs_file)
+        self.proc_multi_ccs_df = pd.read_csv(proc_multi_ccs_file)
 
-        if snomed_cdb_inactive is not None:
-            self.snomed_cdb_inactive_df = pd.read_csv(snomed_cdb_inactive,
-                                                      index_col=0)
-            self.inactive_terms = set(self.snomed_cdb_inactive_df.cui.unique())
+        self.diag_icd_label = self.make_diag_icd_dict()
+        self.proc_icd_label = self.make_proc_icd_dict()
 
-            _df2 = self.snomed_cdb_inactive_df[self.snomed_cdb_inactive_df.tty
-                                               == 1].reset_index(drop=True)
+        (self.diag_single_icd2ccs_dict,
+         self.diag_single_ccs2icd_dict) = self.make_diag_icd2ccs_dict()
+        (self.proc_signle_icd2ccs_dict,
+         self.proc_single_ccs2icd_dict) = self.make_proc_icd2ccs_dict()
 
-            _df2 = (_df2.groupby(
-                'cui',
-                as_index=False).agg(name=('str', lambda x: x.values[0])))
+        (self.diag_multi_ccs_pt2ch, self.diag_multi_icd2ccs,
+         self.diag_multi_ccs2icd) = self.make_diag_multi_dictionaries()
+        (self.proc_multi_ccs_pt2ch, self.proc_multi_icd2ccs,
+         self.proc_multi_ccs2icd) = self.make_proc_multi_dictionaries()
 
-            self.concept_name = {
-                **dict(zip(_df1.cui, _df1.name)),
-                **dict(zip(_df2.cui, _df2.name))
-            }
+    def make_diag_icd_dict(self):
+        diag_ccs_label_list = self.diag_single_ccs_df[
+            '\'ICD-9-CM CODE DESCRIPTION\''].apply(
+                lambda cat: cat.strip('\'').strip()).tolist()
+        diag_ccs_icd_list = self.diag_single_ccs_df['\'ICD-9-CM CODE\''].apply(
+            lambda c: c.strip('\'').strip()).tolist()
+        return dict(zip(diag_ccs_icd_list, diag_ccs_label_list))
 
-        else:
-            self.concept_name = {**dict(zip(_df1.cui, _df1.name))}
+    def make_proc_icd_dict(self):
+        proc_ccs_label_list = self.proc_single_ccs_df[
+            '\'ICD-9-CM CODE DESCRIPTION\''].apply(
+                lambda cat: cat.strip('\'').strip()).tolist()
+        proc_ccs_icd_list = self.proc_single_ccs_df['\'ICD-9-CM CODE\''].apply(
+            lambda c: c.strip('\'').strip()).tolist()
+        return dict(zip(proc_ccs_icd_list, proc_ccs_label_list))
 
-        with open(ch2pt_json) as json_file:
-            self.ch2pt = json.load(json_file)
+    def make_diag_icd2ccs_dict(self):
+        diag_ccs_cat_list = self.diag_single_ccs_df['\'CCS CATEGORY\''].apply(
+            lambda cat: cat.strip('\'').strip()).tolist()
+        diag_ccs_icd_list = self.diag_single_ccs_df['\'ICD-9-CM CODE\''].apply(
+            lambda c: c.strip('\'').strip()).tolist()
 
-        with open(pt2ch_json) as json_file:
-            self.pt2ch = json.load(json_file)
+        diag_icd2ccs_dict = dict(zip(diag_ccs_icd_list, diag_ccs_cat_list))
 
-    def __contains__(self, code):
-        return code in self.ch2pt or code in self.pt2ch
+        diag_ccs2icd_dict = defaultdict(list)
+        for code, cat in zip(diag_ccs_icd_list, diag_ccs_cat_list):
+            diag_ccs2icd_dict[cat].append(code)
 
-    def find_name(self, code):
-        return self.concept_name.get(code, '')
+        return diag_icd2ccs_dict, diag_ccs2icd_dict
 
-    def find_names(self, code):
-        """
-        Converts SNOMED code to Fully specified name and finds any Synonyms
-        """
-        if code in self.active_terms:
-            df = self.snomed_cdb_df[(self.snomed_cdb_df['cui'] == code)
-                                    & (self.snomed_cdb_df['tty'] == 1)]
-            concept_name = df['str'].values
-            return f"{' | '.join(concept_name)}"
-        elif code in self.inactive_terms:
-            df = self.snomed_cdb_inactive_df[
-                (self.snomed_cdb_inactive_df['cui'] == code)
-                & (self.snomed_cdb_inactive_df['tty'] == 1)]
-            concept_name = df['str'].values
-            return f"{' | '.join(concept_name)}"
-        else:
-            return ''
+    def make_proc_icd2ccs_dict(self):
+        proc_ccs_cat_list = self.proc_single_ccs_df['\'CCS CATEGORY\''].apply(
+            lambda cat: cat.strip('\'').strip()).tolist()
+        proc_ccs_icd_list = self.proc_single_ccs_df['\'ICD-9-CM CODE\''].apply(
+            lambda c: c.strip('\'').strip()).tolist()
 
-    def find_syn(self, code):
-        """
-        Converts SNOMED code and finds all Synonyms. Not including concept name
-        """
-        df = self.snomed_cdb_df[(self.snomed_cdb_df['cui'] == code)
-                                & (self.snomed_cdb_df['tty'] == 0)]
-        synonym = df['str'].to_list()
-        return f"{'; '.join(synonym)}"
+        proc_icd2ccs_dict = dict(zip(proc_ccs_icd_list, proc_ccs_cat_list))
 
-    def search_regex(self, query, regex_flags=0):
-        names1 = self.snomed_cdb_df['str']
-        names2 = self.snomed_cdb_inactive_df['str']
-        rows1 = names1.str.contains(query, regex=True, flags=regex_flags, na=False)
-        rows2 = names2.str.contains(query, regex=True, flags=regex_flags, na=False)
+        proc_ccs2icd_dict = defaultdict(list)
+        for code, cat in zip(proc_ccs_icd_list, proc_ccs_cat_list):
+            proc_ccs2icd_dict[cat].append(code)
 
-        codes1 = self.snomed_cdb_df[rows1]
-        codes2 = self.snomed_cdb_inactive_df[rows2]
+        return proc_icd2ccs_dict, proc_ccs2icd_dict
 
-        codes1 = codes1[~codes1.isnull()]
-        codes2 = codes2[~codes2.isnull()]
-        return list(set(codes1.cui) | set(codes2.cui))
+    def make_diag_multi_dictionaries(self):
+        df = self.diag_multi_ccs_df.copy()
+        df['I1'] = df['\'CCS LVL 1\''].apply(lambda l: l.strip('\'').strip())
+        df['I2'] = df['\'CCS LVL 2\''].apply(lambda l: l.strip('\'').strip())
+        df['I3'] = df['\'CCS LVL 3\''].apply(lambda l: l.strip('\'').strip())
+        df['I4'] = df['\'CCS LVL 4\''].apply(lambda l: l.strip('\'').strip())
+        df['L1'] = df['\'CCS LVL 1 LABEL\''].apply(
+            lambda l: l.strip('\'').strip())
+        df['L2'] = df['\'CCS LVL 2 LABEL\''].apply(
+            lambda l: l.strip('\'').strip())
+        df['L3'] = df['\'CCS LVL 3 LABEL\''].apply(
+            lambda l: l.strip('\'').strip())
+        df['L4'] = df['\'CCS LVL 4 LABEL\''].apply(
+            lambda l: l.strip('\'').strip())
+        df['ICD'] = df['\'ICD-9-CM CODE\''].apply(
+            lambda l: l.strip('\'').strip())
 
-    # Get parents of snomed code
-    def get_parents(self, code):
+        df = df[['I1', 'I2', 'I3', 'I4', 'L1', 'L2', 'L3', 'L4']]
+        diag_multi_ccs_pt2ch = defaultdict(list)
+        diag_multi_icd2ccs = {}
+        diag_multi_ccs2icd = defaultdict(list)
+
+        for row in df.itertuples():
+            code = row.ICD
+            l1, l2, l3, l4 = row.L1, row.L2, row.L3, row.L4
+            i1, i2, i3, i4 = row.I1, row.I2, row.I3, row.I4
+
+            last_index = i1
+
+            if i2:
+                diag_multi_ccs_pt2ch[i1].append(i2)
+                last_index = i2
+
+            if i3:
+                diag_multi_ccs_pt2ch[i2].append(i3)
+                last_index = i3
+            if i4:
+                diag_multi_ccs_pt2ch[i3].append(i4)
+                last_index = i4
+
+            diag_multi_icd2ccs[code] = last_index
+            diag_multi_ccs2icd[last_index].append(code)
+
+        return diag_multi_ccs_pt2ch, diag_multi_icd2ccs, diag_multi_ccs2icd
+
+    def make_proc_multi_dictionaries(self):
+        df = self.proc_multi_ccs_df.copy()
+        df['I1'] = df['\'CCS LVL 1\''].apply(lambda l: l.strip('\'').strip())
+        df['I2'] = df['\'CCS LVL 2\''].apply(lambda l: l.strip('\'').strip())
+        df['I3'] = df['\'CCS LVL 3\''].apply(lambda l: l.strip('\'').strip())
+        df['L1'] = df['\'CCS LVL 1 LABEL\''].apply(
+            lambda l: l.strip('\'').strip())
+        df['L2'] = df['\'CCS LVL 2 LABEL\''].apply(
+            lambda l: l.strip('\'').strip())
+        df['L3'] = df['\'CCS LVL 3 LABEL\''].apply(
+            lambda l: l.strip('\'').strip())
+        df['ICD'] = df['\'ICD-9-CM CODE\''].apply(
+            lambda l: l.strip('\'').strip())
+
+        proc_multi_ccs_pt2ch = defaultdict(list)
+        proc_multi_icd2ccs = {}
+        proc_multi_ccs2icd = defaultdict(list)
+
+        for row in df.itertuples():
+            code = row.ICD
+            l1, l2, l3 = row.L1, row.L2, row.L3
+            i1, i2, i3 = row.I1, row.I2, row.I3
+
+            last_index = i1
+
+            if i2:
+                proc_multi_ccs_pt2ch[i1].append(i2)
+                last_index = i2
+
+            if i3:
+                proc_multi_ccs_pt2ch[i2].append(i3)
+                last_index = i3
+
+            proc_multi_icd2ccs[code] = last_index
+            proc_multi_ccs2icd[last_index].append(code)
+
+        return proc_multi_ccs_pt2ch, proc_multi_icd2ccs, proc_multi_ccs2icd
+
+    def find_diag_icd_name(self, code):
+        return self.diag_icd_label[code]
+
+    def find_proc_icd_name(self, code):
+        return self.proc_icd_label[code]
+
+    # Get parents of CCS code
+    def get_ccs_parents(self, ccs_code):
+        indices = ccs_code.split('.')
+        parents = []
+        for i in reversed(range(1, len(indices))):
+            parent = '.'.join(indices[0:i])
+            parents.append(parent)
+        return parents
+
+    # Get all children of CCS code
+    def get_diag_ccs_children(self, ccs_code):
         result = []
-        q = [code]
+        q = [ccs_code]
 
         while len(q) != 0:
             # remove the first element from the stack
-            current_snomed = q.pop(0)
-            current_snomed_children = self.ch2pt.get(current_snomed, [])
-            q.extend([c for c in current_snomed_children if c not in q])
-            result.append(current_snomed)
-        result.remove(code)
+            current_ccs = q.pop(0)
+            expanded_ccs = self.diag_multi_ccs_pt2ch.get(current_ccs, [])
+            q.extend([c for c in expanded_ccs if c not in result])
+            if current_ccs not in result:
+                result.append(current_ccs)
+        result.remove(ccs_code)
         return result
 
-    # Get all children of snomed code
-    def get_children(self, code):
+    # Get all children of CCS code
+    def get_proc_ccs_children(self, ccs_code):
         result = []
-        q = [code]
+        q = [ccs_code]
 
         while len(q) != 0:
             # remove the first element from the stack
-            current_snomed = q.pop(0)
-            current_snomed_parent = self.pt2ch.get(current_snomed, [])
-            q.extend([c for c in current_snomed_parent if c not in result])
-            if current_snomed not in result:
-                result.append(current_snomed)
-        result.remove(code)
+            current_ccs = q.pop(0)
+            expanded_ccs = self.proc_multi_ccs_pt2ch.get(current_ccs, [])
+            q.extend([c for c in expanded_ccs if c not in result])
+            if current_ccs not in result:
+                result.append(current_ccs)
+        result.remove(ccs_code)
         return result
 
-    def children_traversal(self, code):
+    def diag_ccs_children_traversal(self, ccs_code):
         children_set = set()
 
         def _children_traversal(_node):
-            for ch in self.pt2ch.get(_node, []):
+            for ch in self.diag_multi_ccs_pt2ch.get(_node, []):
                 children_set.add(ch)
                 _children_traversal(ch)
 
-        _children_traversal(code)
+        _children_traversal(ccs_code)
         return children_set
 
-    def ancestors_linkage(self, code):
+    def proc_ccs_children_traversal(self, ccs_code):
+        children_set = set()
+
+        def _children_traversal(_node):
+            for ch in self.proc_multi_ccs_pt2ch.get(_node, []):
+                children_set.add(ch)
+                _children_traversal(ch)
+
+        _children_traversal(ccs_code)
+        return children_set
+
+    def ancestors_linkage(self, ccs_code):
         ancestors_set = set()
 
         def _ancestors_linkage(c):
-            for pt in self.ch2pt.get(c, []):
+            for pt in self.get_ccs_parents(c):
                 ancestors_set.add((c, pt))
                 _ancestors_linkage(pt)
 
-        _ancestors_linkage(code)
+        _ancestors_linkage(ccs_code)
         return ancestors_set
 
-    def common_parent(self, snomed1, snomed2):
-        for parent in self.get_parents(snomed1):
-            if parent in self.get_parents(snomed2):
+    def common_parent(self, ccs1, ccs2):
+        for parent in self.get_ccs_parents(ccs1):
+            if parent in self.get_ccs_parents(ccs2):
                 return parent
 
-    def get_parent_from_list(self, snomed_list):
-        if len(snomed_list) == 2:
-            return self.common_parent(snomed_list[0], snomed_list[1])
-        else:
-            first_snomed = snomed_list[0]
-            rest_of_snomed = snomed_list[1:]
-            rest_of_snomed_parent = self.get_parent_from_list(rest_of_snomed)
-            return self.common_parent(first_snomed, rest_of_snomed_parent)
-
-    def prune_subsets(self, branches):
-        branches = [[c, s] for c, s in branches.items()]
-
-        branches = sorted(branches, key=lambda t: len(t[1]))
-
-        for i in range(len(branches)):
-            i_code, i_set = branches[i]
-            i_parents = set(self.get_parents(i_code))
-            for j in range(i + 1, len(branches)):
-                j_code, j_set = branches[j]
-                if j_code in i_parents:
-                    branches[j][1] = j_set - i_set
-
-        return {c: s for c, s in branches}
-
-    def dag_from_dataframe(self, df, discard_set=None, node_attrs=None):
+    def digraph_from_dataframe(self, df, node_attrs=None):
+        # TODO: DataFrame will have ICD, CCS_MULTI, CCS_SINGLE columns
         _ch2pt = set()
         dag = nx.DiGraph()
 
-        def parents_traversal(node):
-            for pt in self.ch2pt.get(node, []):
-                _ch2pt.add((node, pt))
-                parents_traversal(pt)
+        def parents_traversal(ccs_code):
+            parents = self.get_ccs_parents(ccs_code)
+            _ch2pt.update(zip([ccs_code] + parents[:-1], parents))
 
-        if discard_set:
-            pruned_df = df[df.core_code.apply(lambda c: c not in discard_set)]
-            pruned_df.core_code.apply(lambda c: parents_traversal(c))
-        else:
-            df.core_code.apply(lambda c: parents_traversal(c))
-
-        for ch, pt in _ch2pt:
-            dag.add_edge(ch, pt)
-
-        if node_attrs is not None:
-            for node in dag.nodes:
-                for attr_name, attr_dict in node_attrs.items():
-                    dag.nodes[node][attr_name] = attr_dict.get(node, '')
-        return dag
-
-    def digraph_from_dataframe(self, df, discard_set=None, node_attrs=None):
-        _ch2pt = set()
-        dag = nx.DiGraph()
-
-        def parents_traversal(node):
-            for pt in self.ch2pt.get(node, []):
-                _ch2pt.add((node, pt))
-                parents_traversal(pt)
-
-        if discard_set:
-            pruned_df = df[df.core_code.apply(lambda c: c not in discard_set)]
-            pruned_df.core_code.apply(lambda c: parents_traversal(c))
-        else:
-            df.core_code.apply(lambda c: parents_traversal(c))
+        df['CCS_MULTI'].apply(lambda c: parents_traversal(c))
 
         for ch, pt in _ch2pt:
             dag.add_edge(ch, pt)
