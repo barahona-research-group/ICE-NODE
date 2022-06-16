c77441ae43b31db06fbeb98fac0fc8881c1fcc81
Asem Alaa <asem_alla@msn.com>
2022-06-16 22:18:06 +0100

    update

--- hpc/doc-jobs/wconf-job.sh
+++ hpc/doc-jobs/wconf-job.sh
@@ -58,7 +58,7 @@ fi
 $HOME/GP/env/icenode-env/bin/python -m icenode.ehr_predictive.train_app \
 --config $CONFIG \
 --study-tag $STUDY_TAG \
---config-tag $TAG \
+--config-tag $CONFIG_TAG \
 --output-dir $OUTPUT_DIR \
 --mimic-processed-dir $DATA_DIR \
 --emb $EMB \
