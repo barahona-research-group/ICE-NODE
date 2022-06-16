fca74cb06d2bcb6102c2e9d2d02aa9b673d19be9
Asem Alaa <asem_alla@msn.com>
2022-06-16 13:03:23 +0100

    update

--- test/integration/ehr_predictive/test_dx.py
+++ test/integration/ehr_predictive/test_dx.py
@@ -74,7 +74,7 @@ class TestDxWindowLogReg(DxCommonTests, unittest.TestCase):
         ConvergenceWarning('ignore')
         sklearn_trainer(model=model,
                         m_state=m_state,
-                        config=config,
+                        config=m_config,
                         splits=splits,
                         reporters=[MinibatchLogger()])
 
