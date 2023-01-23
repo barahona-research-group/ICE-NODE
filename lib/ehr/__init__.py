e4de6ea1388f6f7fbf912894d0b518758ad236f0
Asem Alaa <asem.a.abdelaziz@gmail.com>
2023-01-23 22:59:18 +0000

    more on notebook

--- lib/ehr/__init__.py
+++ lib/ehr/__init__.py
@@ -1,6 +1,6 @@
 from .dataset import (load_dataset, AbstractEHRDataset, MIMIC3EHRDataset,
                       MIMIC4EHRDataset, CPRDEHRDataset)
-from .concept import Subject, Admission
+from .concept import Subject, Admission, StaticInfoFlags
 from .jax_interface import (Subject_JAX, Admission_JAX, BatchPredictedRisks)
 from .coding_scheme import AbstractScheme
 from .outcome import OutcomeExtractor, outcome_conf_files, FirstOccurrenceOutcomeExtractor
