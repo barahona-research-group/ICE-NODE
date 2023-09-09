1233e3e1671c781d4a2c4b079e5d69e75e11dde5
A-Alaa <asem_alla@msn.com>
2023-09-09 18:46:05 +0100

    update

--- hpc/doc-jobs/wconf-job.sh
+++ hpc/doc-jobs/wconf-job.sh
@@ -13,7 +13,7 @@ export MLFLOW_STORE="file://${STORE}/mlflow-store"
 
 # Input Environment Variables:
 # $STUDY_TAG: Name of the Git branch or tag. If optuna job is executing, the study tag will be used for the optuna study name. Example: v0.2.25
-# $CONFIG: Path (absolute or relative) to JSON config file for the experiment settings. Example: ~/GP/ICE-NODE/optimal_configs/icenode_v1/icenode_2lr.json
+# $CONFIG_PATH: Path (absolute or relative) to JSON config file for the experiment settings. Example: ~/GP/ICE-NODE/optimal_configs/icenode_v1/icenode_2lr.json
 # $CACHE_PATH: Path (absolute or relative) to the cache directory. Example: ~/GP/ICE-NODE/cache
 # $OUTPUT_PATH: Path (absolute or relative) to the output directory. Example: ~/GP/ICE-NODE/output
 # $DATASET_PATH: Path (absolute or relative) to the dataset directory. Example: ~/GP/ehr-data/dataset
@@ -35,7 +35,7 @@ source /vol/cuda/11.4.120-cudnn8.2.4/setup.sh
 
 
 $HOME/GP/env/icenode-dev/bin/python -m lib.cli.run_config \
---config $env_config_path \
+--config $CONFIG_PATH \
 --output-path $OUTPUT_PATH \
 --dataset-path $DATASET_PATH \
 --cache-path $CACHE_PATH \
--- hpc/local-jobs/wconf-job-gpu.sh
+++ hpc/local-jobs/wconf-job-gpu.sh
@@ -2,7 +2,7 @@
 
 # Input Environment Variables:
 # $STUDY_TAG: Name of the Git branch or tag. If optuna job is executing, the study tag will be used for the optuna study name. Example: v0.2.25
-# $CONFIG: Path (absolute or relative) to JSON config file for the experiment settings. Example: ~/GP/ICE-NODE/optimal_configs/icenode_v1/icenode_2lr.json
+# $CONFIG_PATH: Path (absolute or relative) to JSON config file for the experiment settings. Example: ~/GP/ICE-NODE/optimal_configs/icenode_v1/icenode_2lr.json
 # $CACHE_PATH: Path (absolute or relative) to the cache directory. Example: ~/GP/ICE-NODE/cache
 # $OUTPUT_PATH: Path (absolute or relative) to the output directory. Example: ~/GP/ICE-NODE/output
 # $DATASET_PATH: Path (absolute or relative) to the dataset directory. Example: ~/GP/ehr-data/dataset
@@ -24,7 +24,7 @@ export JAX_PLATFORM_NAME="gpu"
 MLFLOW_STORE="file://${HOME}/GP/ehr-data/mlflow-store"
 
 $HOME/GP/env/icenode-dev/bin/python -m lib.cli.run_config \
---config $env_config_path \
+--config $CONFIG_PATH \
 --output-path $OUTPUT_PATH \
 --dataset-path $DATASET_PATH \
 --cache-path $CACHE_PATH \
