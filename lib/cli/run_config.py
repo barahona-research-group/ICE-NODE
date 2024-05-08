28cdf9bddd889df6db7b8f271289ba38bca67f76
Asem <asem_alla@msn.com>
2024-05-08 15:58:04 +0100

    update jobs

--- lib/cli/run_config.py
+++ lib/cli/run_config.py
@@ -1,14 +1,15 @@
 import argparse
 import logging
+
+from ..base import Config
 from ..ml.experiment import Experiment
 from ..utils import load_config, translate_path
-from ..base import Config
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type=str, required=True)
-    parser.add_argument('--dataset-path', type=str, required=False, default="")
+    parser.add_argument('--dataset-path', type=str, required=True)
     parser.add_argument('--output-path', type=str, required=True)
     parser.add_argument('--override', type=str, required=False, default="")
     args = parser.parse_args()
@@ -20,11 +21,6 @@ if __name__ == '__main__':
 
     config = config.path_update('reporting.output_dir',
                                 translate_path(args.output_path))
-
-    if args.dataset_path is not None and len(args.dataset_path) > 0:
-        config = config.path_update('dataset.path',
-                                    translate_path(args.dataset_path))
-
 
     if args.override is not None and len(
             args.override) > 0 and args.override != '0':
@@ -40,5 +36,4 @@ if __name__ == '__main__':
             config = config.path_update(key, value)
 
     experiment = Experiment(config)
-    experiment.run(tvx_ehr_path=config.dataset.path, prng_seed=42)
-
+    experiment.run(tvx_ehr_path=args.dataset_path, prng_seed=42)
